f34dcf42bdc36af4ca6893b4cc152ece0d655394
davis.trem <davis.trem@gmail.com>
2024-06-03 18:54:09 -0400

    set main menu as boot screen

--- client/scripts/main_menu.gd
+++ client/scripts/main_menu.gd
@@ -1,11 +1,19 @@
 extends Control
 
+@onready var single_player_button: Button = $MarginContainer/VBoxContainer/VBoxContainer/SinglePlayerButton
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+
+func _ready() -> void:
+	single_player_button.grab_focus()
+
+
+func _on_single_player_button_pressed() -> void:
+	get_tree().change_scene_to_file('res://screens/game_play_screen.tscn')
+
+
+func _on_multi_player_button_pressed() -> void:
+	get_tree().change_scene_to_file('res://screens/game_play_screen.tscn')
+
+
+func _on_settings_button_pressed() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
